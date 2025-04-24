38d6b5b7470e0fafd1de6fb37c4b278d6fff3de1
Sena <dimatesebas73@gmail.com>
2025-04-24 13:27:02 -0500

    create the app with your enlaces in to the Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+
+import Header from '../src/Layouts/Header/Header';
+import Footer from '../src/Layouts/Footer/Footer';
+
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <div className="min-h-screen flex flex-col bg-gray-50 text-gray-800">
+  <Header />
+
+  <Footer />
+</div>  
+  );
 }
