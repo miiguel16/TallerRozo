6174e677b181b93fccaf231507e00196b24d9bec
Sena <dimatesebas73@gmail.com>
2025-04-24 13:16:13 -0500

    create the proyect, organization, footer and header

---
+++ src/Layouts/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer className="bg-indigo-100 text-gray-600 text-center p-4 mt-auto text-sm border-t">
+            <div className="container mx-auto">
+                <p>Desarrollado por: <a href="" className="text-indigo-600 hover:text-indigo-800">Miguel Angel Chilito</a></p>
+            </div>
+      </footer>
+      
+    );
+  }
+  
---
+++ src/Layouts/Header/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="bg-indigo-600 text-white p-4 shadow-md text-2xl font-bold text-center">
+             Mi Tienda React Taller Evaluativo
+        </header>
+
+    );
+}
